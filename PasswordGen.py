55b93b9cd8a5ca6635d6531b50db4d3a32022356
gregorymcintyre <gregmcintyre1984@gmail.com>
2019-10-24 17:00:53 +1100

    UPDATE: Array implemented and clipbard function

--- PasswordGen.py
+++ PasswordGen.py
@@ -2,6 +2,7 @@
 
 import random
 import string
+import pyperclip
 
 def randomStringwithDigitsAndSymbols(stringLength=16):
     """Generate a random string of letters, digits and special characters """
@@ -9,16 +10,20 @@ def randomStringwithDigitsAndSymbols(stringLength=16):
     password_characters = string.ascii_letters + string.digits + string.punctuation
     return ''.join(random.choice(password_characters) for i in range(stringLength))
 
-print("Generating Random String password with letters, digits and special characters ")
-print (randomStringwithDigitsAndSymbols() )
-print (randomStringwithDigitsAndSymbols(16) )
-print (randomStringwithDigitsAndSymbols(16) )
-print (randomStringwithDigitsAndSymbols(16) )
-print (randomStringwithDigitsAndSymbols(16) )
-print (randomStringwithDigitsAndSymbols(16) )
-print (randomStringwithDigitsAndSymbols(16) )
-print (randomStringwithDigitsAndSymbols(16) )
-print (randomStringwithDigitsAndSymbols(16) )
-print (randomStringwithDigitsAndSymbols(16) )
+print("Generating Random String password with letters, digits and special characters \n")
+#print (randomStringwithDigitsAndSymbols() )
 
-input("Press enter to exit")
+pwds = []
+
+for x in range(10):
+	pwds.append(randomStringwithDigitsAndSymbols(16))
+
+for x in range(10):
+	print(str(x) + ". " + pwds[x])
+
+choice = input("\nChoose password number: ")
+pyperclip.copy(pwds[int(choice)])
+print("Password copied to clipboard!")
+
+input("\nPress enter to exit")
+
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # SimplePasswordGen
 Simple Password Generator
+Greg McIntyre
+24/10/2019
+
+Generates 10 Passwords, copies selected to clipboard.
+
+
+Prerequisite
+$ sudo apt install xsel
+$ sudo apt install python3-pip
+$ sudo pip3 install pyperclip
+
+Run
+SimplePasswordGen/PasswordGen.py
